5390991dfe7a25118bdfffff673e50ad3cacb472
MasiaVittorio <masiavivittorio@gmail.com>
2023-07-02 15:08:40 +0200

    updated dependencies

--- example/lib/widgets/home.dart
+++ example/lib/widgets/home.dart
@@ -1,6 +1,5 @@
 import 'package:example/logic/theme_logic.dart';
 import 'package:flutter/material.dart';
-import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:sid_base/sid_base.dart';
 
 class HomeExample extends StatelessWidget {
@@ -19,7 +18,7 @@ class HomeExample extends StatelessWidget {
           value: value, 
           onChanged: (_) => themeLogic.toggleBrightness(),
           title: const Text("Dark mode:"),
-          secondary: const Icon(MdiIcons.weatherNight),
+          secondary: Icon(MdiIcons.weatherNight),
         )),
       ],),
     );
--- lib/src/all.dart
+++ lib/src/all.dart
@@ -1,3 +1,4 @@
 export 'state/all.dart';
 export 'utils/all.dart';
 export 'ui/all.dart';
+export 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
