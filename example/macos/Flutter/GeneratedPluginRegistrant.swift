5361032c290bd89bde3c7bce1234b3e2c7771bf7
Vittorio Masia <14254644+MasiaVittorio@users.noreply.github.com>
2025-03-04 21:34:05 +0100

    dependencies

--- example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,7 +10,7 @@ import dynamic_color
 import firebase_auth
 import firebase_core
 import path_provider_foundation
-import sqflite
+import sqflite_darwin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
